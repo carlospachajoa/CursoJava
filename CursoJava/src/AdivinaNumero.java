52d9af56219e31c355ee17bdf489a649189b1b76
carlos.pachajoa <carlos.pachajoa@pas-w-antel.ucc.nal>
2017-08-03 16:00:53 -0500

    2

--- CursoJava/src/AdivinaNumero.java
+++ CursoJava/src/AdivinaNumero.java
@@ -4,8 +4,28 @@ public class AdivinaNumero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int aleatorio=(int)(Math.random()*100);
 		
-
+		Scanner entrada =new Scanner(System.in);
+		int numero=0;
+		int intentos=0;
+		
+		while(numero!=aleatorio) {
+			intentos++;
+			System.out.println("Introduce numero");
+			numero=entrada.nextInt();
+			if (aleatorio<numero) {
+				System.out.println("Mas bajo");
+			}
+			else if (aleatorio>numero)
+			{
+				System.out.println("Mas alto");
+			}
+			
+		}
+		System.out.println("Correcto lo consiguio en "+ intentos + " Intentos");
+	entrada.close();
 	}
+	
 
 }
