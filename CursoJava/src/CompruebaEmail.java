d1f5cd444cf165ad093a45759d6a1b84d2dff7ec
carlos.pachajoa <carlos.pachajoa@pas-w-antel.ucc.nal>
2017-08-08 10:06:50 -0500

    1

--- CursoJava/src/CompruebaEmail.java
+++ CursoJava/src/CompruebaEmail.java
@@ -4,16 +4,20 @@ public class CompruebaEmail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean arroba =false;
+		int arroba =0;
+		boolean punto=false;
 		
 		String mail= JOptionPane.showInputDialog("Introducir Email :");
 		for(int i=0;i<mail.length();i++) {
 			if(mail.charAt(i)=='@') {
-				arroba=true;
+				arroba++;
+			}
+			if(mail.charAt(i)=='.') {
+				punto=true;				
 			}
 			
 		}
-		if (arroba==true){
+		if (arroba==1 && punto==true){
 			System.out.println("Es correcto");
 		}else
 		{
