ee87be330300167cb7265032e1d85a249df3cd13
carlos.pachajoa <carlos.pachajoa@pas-w-antel.ucc.nal>
2017-08-08 17:17:09 -0500

    5

---
+++ CursoJava/src/UsoArray2.java
@@ -0,0 +1,22 @@
+
+public class UsoArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*String [] paises=new String [8];
+		paises[0]="Colombia";
+		paises[1]="Ecuador";
+		paises[2]="Mexico";
+		paises[3]="Peru";
+		paises[4]="Chile";*/
+		
+		String [] paises = {"Colombia","Ecuador","España","Chile"};
+		
+		for(String elemento:paises) {
+			System.out.println(elemento);
+		}
+		
+		
+	}
+
+}
--- CursoJava/src/UsoArrays.java
+++ CursoJava/src/UsoArrays.java
@@ -3,14 +3,15 @@ public class UsoArrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] mi_matriz= new int[5];
+		/*int [] mi_matriz= new int[5];
 		mi_matriz[0]=5;
 		mi_matriz[1]=23;
 		mi_matriz[2]=54;
 		mi_matriz[3]=78;
-		mi_matriz[4]=51;
+		mi_matriz[4]=51;*/
+		int [] mi_matriz= {5,6,7,9,12,8,65,75,34,78,56};
 		
-		for(int i=0;i<5;i++)
+		for(int i=0;i<mi_matriz.length;i++)
 		{
 			System.out.println("Valor indice "+i+" = "+mi_matriz[i]);
 		}
