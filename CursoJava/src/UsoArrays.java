a43987108a61d08b8bbe76fc6f34ac10ab932630
carlos.pachajoa <carlos.pachajoa@pas-w-antel.ucc.nal>
2017-08-08 15:37:22 -0500

    3

---
+++ CursoJava/src/UsoArrays.java
@@ -0,0 +1,21 @@
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] mi_matriz= new int[5];
+		mi_matriz[0]=5;
+		mi_matriz[1]=23;
+		mi_matriz[2]=54;
+		mi_matriz[3]=78;
+		mi_matriz[4]=51;
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Valor indice "+i+" = "+mi_matriz[i]);
+		}
+		
+		
+	}
+
+}
